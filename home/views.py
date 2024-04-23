6570f50d94158f1e9a6b99b1499c2947defcbab5
unknown <ozhan728@gmail.com>
2024-04-24 01:04:48 +0330

    wrote my first API

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
-class Home(View):
+class Home(APIView):
     def get(self,request):
-        return render(request,'home/home.html')
+        # name = request.GET['name']
+        # name = request.query_params['name']
+        return Response({'age':26,'name':'ozhan'})
+
+    def post(self,request):
+        name = request.data['name']
+        return Response({'name':name})
