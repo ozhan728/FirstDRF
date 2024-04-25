d9671503c4fe8c1f70236968f1638d719422c146
unknown <ozhan728@gmail.com>
2024-04-26 00:25:12 +0330

    implementend registering through api

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=25)
+    age = models.PositiveSmallIntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.name} - {self.email} '
--- home/views.py
+++ home/views.py
@@ -1,13 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Person
+from .serializers import PersonSerializer
 
 # Create your views here.
 class Home(APIView):
     def get(self,request):
         # name = request.GET['name']
         # name = request.query_params['name']
-        return Response({'age':26,'name':'ozhan'})
+        persons = Person.objects.all()
+        ser_data = PersonSerializer(instance=persons,many=True)
+        return Response(data=ser_data.data)
 
-    def post(self,request):
-        name = request.data['name']
-        return Response({'name':name})
+    # def post(self,request):
+    #     name = request.data['name']
+    #     return Response({'name':name})
