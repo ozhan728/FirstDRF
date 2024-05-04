33c3da4c1540921cbff1286c5d31357a54aa1c73
unknown <ozhan728@gmail.com>
2024-05-04 15:40:29 +0330

    added status codes

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserRegisterSerializers
+from rest_framework import status
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -10,10 +11,11 @@ class UserRegister(APIView):
     def post(self,request):
         ser_data = UserRegisterSerializers(data=request.POST)
         if ser_data.is_valid():
-            User.objects.create_user(
-                username=ser_data.validated_data['username'],
-                email = ser_data.validated_data['email'],
-                password = ser_data.validated_data['password'],
-            )
-            return Response(ser_data.data)
-        return Response(ser_data.errors)
+            ser_data.create(ser_data.validated_data)
+            # User.objects.create_user(
+            #     username=ser_data.validated_data['username'],
+            #     email = ser_data.validated_data['email'],
+            #     password = ser_data.validated_data['password'],
+            # )
+            return Response(ser_data.data,status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors,status=status.HTTP_400_BAD_REQUEST)
