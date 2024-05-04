3b2cc82abfad4ba0c0b758a9f24db26f029a106f
unknown <ozhan728@gmail.com>
2024-05-04 23:54:54 +0330

    implemented token url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken import views as auth_token
 
 app_name = 'accounts'
 
 urlpatterns = [
 
     path('register/',views.UserRegister.as_view()),
+    path('api-token-auth/',auth_token.obtain_auth_token)
 
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- drf/settings.py
+++ drf/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'accounts.apps.AccountsConfig',
     'rest_framework',
+    'rest_framework.authtoken',
 
 ]
 
