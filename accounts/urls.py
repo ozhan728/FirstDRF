2a15b6b4fa2f125121a7c0fe0d48f671dcd011e0
unknown <ozhan728@gmail.com>
2024-05-14 02:09:16 +0330

    switched to jwt

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from . import views
-from rest_framework.authtoken import views as auth_token
+# from rest_framework.authtoken import views as auth_token
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'accounts'
 
 urlpatterns = [
 
     path('register/',views.UserRegister.as_view()),
-    path('api-token-auth/',auth_token.obtain_auth_token),
+    # path('api-token-auth/',auth_token.obtain_auth_token),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
 
@@ -17,3 +23,8 @@ router = routers.SimpleRouter()
 router.register('user',views.UserViewSet)
 
 urlpatterns += router.urls
+
+
+# {
+#   "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcxNTcyNTY3MiwiaWF0IjoxNzE1NjM5MjcyLCJqdGkiOiI4NTY4M2ZmZTMxMzg0MjQxODM1NjA5OTZmMDJiMjI1NiIsInVzZXJfaWQiOjF9.6GFXNQeZQ-PoagRTirmpozepKRGd9daxBwV4oIThZAU",
+# }
--- drf/settings.py
+++ drf/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'accounts.apps.AccountsConfig',
     'rest_framework',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
 
 ]
 
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
