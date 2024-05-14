59da226c340d5060e6b22edd6f8356c41c75757a
unknown <ozhan728@gmail.com>
2024-05-15 01:30:13 +0330

    added renderers

--- drf/settings.py
+++ drf/settings.py
@@ -144,6 +144,14 @@ REST_FRAMEWORK = {
 
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.AdminRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.StaticHTMLRenderer',
+
+    ]
+
 }
 
 
